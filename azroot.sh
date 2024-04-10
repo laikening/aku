0c5b3b445f21a2d3570835ea27553f8749911a0f
laikening <84102491+laikening@users.noreply.github.com>
2024-04-11 03:38:35 +0800

    Update azroot.sh

--- azroot.sh
+++ azroot.sh
@@ -1,3 +1,4 @@
+sudo -i
 sed -i 's/^.*PermitRootLogin.*/PermitRootLogin yes/g' /etc/ssh/sshd_config;
 sed -i 's/^.*PasswordAuthentication.*/PasswordAuthentication yes/g' /etc/ssh/sshd_config;
 service sshd restart
