c6a729130a95ac3e1f8a4fb797f3a652c10374a2
laikening <84102491+laikening@users.noreply.github.com>
2025-07-09 21:32:01 +0800

    Update GDK.sh

--- GDK.sh
+++ GDK.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 cd  /opt/xray2025/bin/
-find . -type f -name '*.json' -exec sed -i 's/8787/8989/g' {} +
+find . -type f -name '*.json' -exec sed -i 's/888/gw888/g' {} +
 systemctl daemon-reload
 systemctl enable xray2025
 systemctl restart xray2025
